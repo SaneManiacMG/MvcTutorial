2947a0544f83c0e73de0bffcbcb09cf51bef472b
SaneManiacMG <mmoroane@hotmail.com>
2023-04-21 08:45:58 +0200

    Init

---
+++ MvcTutorial/MvcTutorial/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcTutorial.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action";
+        }
+
+        public string Welcome()
+        {
+            return "This is the welcome method";
+        }
+    }
+}
